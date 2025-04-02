e76148f76979c6a30231a90069cf1f6c90abc7bc
Andrew Jensen <andrewjensen90@gmail.com>
2025-04-02 16:37:52 -0600

    WIP: Block out component structure and get basic styles in place

--- src/components/EntityTasksPage.tsx
+++ src/components/EntityTasksPage.tsx
@@ -8,12 +8,12 @@ import {
 import { useApi } from "@backstage/core-plugin-api";
 import { useEntity } from "@backstage/plugin-catalog-react";
 import Box from "@material-ui/core/Box";
-// import Card from "@material-ui/core/Card";
+import Card from "@material-ui/core/Card";
 import Grid from "@material-ui/core/Grid";
 import useAsync from "react-use/lib/useAsync";
 
-import { dxApiRef } from "../api";
-// import { COLORS } from "../styles";
+import { dxApiRef, Task } from "../api";
+import { COLORS } from "../styles";
 import { PoweredByDX } from "./Branding";
 
 export function EntityTasksPage() {
@@ -74,10 +74,192 @@ export function EntityTasksPage() {
         </ContentHeader>
       </Box>
       <Grid container spacing={3} alignItems="stretch">
-        <div>
-          <pre>{JSON.stringify(tasks, null, 2)}</pre>
-        </div>
+        {[0, 1, 2].map((priorityLevel) => (
+          <Grid item xs={12} key={priorityLevel}>
+            <PriorityTaskList
+              priorityLevel={priorityLevel}
+              tasks={tasks.filter(
+                (task) => task.initiative_priority === priorityLevel
+              )}
+            />
+          </Grid>
+        ))}
       </Grid>
     </>
   );
+}
+
+function PriorityTaskList({
+  priorityLevel,
+  tasks,
+}: {
+  priorityLevel: number;
+  tasks: Task[];
+}) {
+  if (tasks.length === 0) {
+    return null;
+  }
+
+  return (
+    <Card>
+      <Box
+        sx={{
+          height: 60,
+          paddingLeft: 24,
+          paddingRight: 24,
+          display: "flex",
+          alignItems: "center",
+          gridGap: 8,
+          borderBottom: `1px solid ${COLORS.GRAY_200}`,
+        }}
+      >
+        <PriorityIndicator priorityLevel={priorityLevel} />
+        <Box
+          sx={{
+            fontSize: 22,
+            fontWeight: 700,
+            letterSpacing: "-0.25px",
+            lineHeight: "normal",
+          }}
+        >
+          P{priorityLevel}
+        </Box>
+      </Box>
+      <Box>
+        {tasks.map((task) => (
+          <Box
+            key={`${task.check_id}-${task.initiative_id}`}
+            sx={{ borderBottom: `1px solid ${COLORS.GRAY_200}` }}
+          >
+            <TaskSummary task={task} />
+          </Box>
+        ))}
+      </Box>
+      <Box
+        sx={{
+          paddingLeft: 24,
+          paddingRight: 24,
+          paddingTop: 12,
+          paddingBottom: 12,
+        }}
+      >
+        TODO: View tasks in DX
+      </Box>
+    </Card>
+  );
+}
+
+function PriorityIndicator({ priorityLevel }: { priorityLevel: number }) {
+  // TODO: Move to styles, use from EntityTasksCard too
+  const PRIORITY_COLORS: Record<number, { bg: string; fg: string }> = {
+    0: {
+      bg: COLORS.RED_50,
+      fg: COLORS.RED_600,
+    },
+    1: {
+      bg: COLORS.ORANGE_50,
+      fg: COLORS.ORANGE_600,
+    },
+    2: {
+      bg: COLORS.AMBER_50,
+      fg: COLORS.AMBER_600,
+    },
+  };
+
+  const indicatorColor = PRIORITY_COLORS[priorityLevel].fg;
+  return (
+    <div
+      style={{
+        width: 8,
+        height: 8,
+        borderRadius: 24,
+        marginBottom: 1,
+        backgroundColor: indicatorColor,
+      }}
+    />
+  );
+}
+
+function TaskSummary({ task }: { task: Task }) {
+  const formattedDueDate = formatDueDate(task.initiative_complete_by);
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        gridGap: 16,
+        padding: 16,
+      }}
+    >
+      <Box
+        sx={{ flex: 1, display: "flex", flexDirection: "column", gridGap: 12 }}
+      >
+        <Box sx={{ fontWeight: 500, fontSize: 14, color: "#030712" }}>
+          {task.check_name}
+        </Box>
+        <Box>
+          <Box sx={{ fontSize: 13, fontWeight: 500, lineHeight: "20px" }}>
+            {task.initiative_name}
+          </Box>
+          <div
+            style={{
+              marginTop: 2,
+              fontSize: 12,
+              color: "#7f7f7f",
+              lineHeight: "normal",
+              display: "-webkit-box",
+              WebkitLineClamp: 2,
+              WebkitBoxOrient: "vertical",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+            }}
+          >
+            {task.initiative_description}
+          </div>
+        </Box>
+      </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "start",
+          gridGap: 16,
+          fontSize: 13,
+          color: COLORS.UI_GRAY_40,
+          whiteSpace: "nowrap",
+        }}
+      >
+        <Box>Requested by {task.owner.name}</Box>
+        <Box sx={{ display: "flex", alignItems: "center", gridGap: 5 }}>
+          <TimeIcon />
+          <span style={{ whiteSpace: "nowrap" }}>Due {formattedDueDate}</span>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+// TODO: Extract and reuse
+function TimeIcon() {
+  // The SVG for this icon was having major aliasing problems, so we're inlining it as a PNG instead
+  return (
+    <img
+      src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAADiSURBVHgBlZDLDsFAFIbHJWGNhC0SK8FeQkLY8U5io4+hTViIpG3UQiJpqb3iPeoNhv+0004jFv2Sycyc+38y/AtLQVb+OBeXjadzVqk1WL5YpreqbZMZPGSxXPFmq8dtx+W+/yabblhkg09ACWt1Qw4RiCQcAJsoFCWMJjOumxaXu8lVEYwYkA9mv7HzyfwrtNtpM+/xijWUq/VoHHD3njQGbkGuUKI7+1Mh/O93GnUGhnlkw0E/7oAZZdEyQjQWE4kOhCrk0I1DFGg713CtSnKtAlTBNjAvdOEt1inIUFYKPkrvLf/n5L74AAAAAElFTkSuQmCC"
+      alt="Time icon"
+      style={{
+        marginBottom: 2,
+      }}
+    />
+  );
+}
+
+function formatDueDate(date: string): string {
+  return new Date(date).toLocaleDateString(undefined, {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+    timeZone: "UTC",
+  });
 }
