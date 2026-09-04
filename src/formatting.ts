07a470541ae3214aeace1bc81a264489cec98ac4
Andrew Jensen <andrewjensen90@gmail.com>
2026-09-03 19:46:43 -0600

    Fix lint error

--- src/formatting.ts
+++ src/formatting.ts
@@ -3,7 +3,7 @@ import pluralizeLib from "pluralize";
 const pluralRules = new Intl.PluralRules("en-US");
 
 export function pluralize(text: string, count: number) {
-  if (count != null && pluralRules.select(count) === "one") {
+  if (count !== null && pluralRules.select(count) === "one") {
     return pluralizeLib.singular(text);
   }
   return pluralizeLib.plural(text);
